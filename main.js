a0c647755c604f4d2d4ac8301b73eec1b02d3a3c
Virgilio Sardido Reyes <65864484+vsreyes@users.noreply.github.com>
2021-04-04 17:21:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,159 @@
+const path = require('path');
+const os = require('os');
+const { app, BrowserWindow, Menu, ipcMain, shell } = require('electron');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
+const log = require('electron-log');
+
+// Set env
+process.env.NODE_ENV = 'production';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isWin = process.platform === 'win32' ? true : false;
+
+let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev ? true : false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  /*mainWindow.loadURL(`file://${__dirname}/app/index.html`);*/
+  mainWindow.loadFile('./app/index.html');
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: 'white',
+  });
+
+  aboutWindow.loadFile('./app/about.html');
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isWin
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: 'fileMenu',
+    /*label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: isWin ? 'Shift+w' : 'Ctrl+w',
+        click: () => app.quit(),
+      },
+    ],*/
+  },
+  /*...(isWin
+    ? [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),*/
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
+
+ipcMain.on('image:minimize', (e, options) => {
+  options.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(options);
+});
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+
+    log.info(files);
+
+    //shell.openItem(dest);
+
+    shell.openPath(dest);
+
+    mainWindow.webContents.send('image:done');
+  } catch (err) {
+    console.log(err);
+    log.error(err);
+  }
+}
+
+app.on('window-all-closed', () => {
+  if (!isWin) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if ((BrowserWindow, getAllWindows().length === 0)) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
